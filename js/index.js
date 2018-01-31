3dad77afd1b5e062b4a325c0857c58a2dc9102b8
Lucas Bersier <bers.lucas@gmail.com>
2018-01-30 19:43:06 -0500

    Initial proof-of-concept

--- js/index.js
+++ js/index.js
@@ -1,9 +1,43 @@
-var howmuchis = new Vue({
+let howmuchis = new Vue({
   el: '#app',
   data: {
-    coinList: []
+    coinList: [],
+    selected: {},
+    test: 'data',
+    result: '',
   },
   methods: {
-    
+    calc: function() {
+      axios.get('https://api.coinmarketcap.com/v1/ticker/' + howmuchis.selected.coin + '/')
+        .then(function(response) {
+          howmuchis.coinList = howmuchis.coinList.map(function(coin){
+            coin.price_usd = (coin.id === howmuchis.selected.coin) ?
+              response.data[0].price_usd :
+              null;
+            return coin;
+          });
+
+          howmuchis.result = parseInt(response.data[0].price_usd) * howmuchis.selected.amount;
+        })
+    }
   }
-})
+});
+
+howmuchis.coinList = [
+  {
+    id: 'bitcoin',
+    desc: 'Bitcoin',
+  },
+  {
+    id: 'monero',
+    desc: 'Monero'
+  },
+  {
+    id: 'ethereum',
+    desc: 'Ethereum'
+  },
+  {
+    id: 'raiblocks',
+    desc: 'Raiblocks'
+  }
+];
