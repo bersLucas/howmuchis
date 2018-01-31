13e0eab6d68cae70dbd4e918c6565bba8456fbd0
Lucas Bersier <bers.lucas@gmail.com>
2018-01-30 23:01:58 -0500

    Style (wip), Adding more inputs, and using coin objects

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ let howmuchis = new Vue({
   el: '#app',
   data: {
     coinList: [],
-    selected: {},
+    inputs: [{
+      coin: 'placeholder'
+    }],
     test: 'data',
     result: '',
   },
@@ -10,7 +12,7 @@ let howmuchis = new Vue({
     calc: function() {
       axios.get('https://api.coinmarketcap.com/v1/ticker/' + howmuchis.selected.coin + '/')
         .then(function(response) {
-          howmuchis.coinList = howmuchis.coinList.map(function(coin){
+          howmuchis.coinList = howmuchis.coinList.map(function(coin) {
             coin.price_usd = (coin.id === howmuchis.selected.coin) ?
               response.data[0].price_usd :
               null;
@@ -19,25 +21,32 @@ let howmuchis = new Vue({
 
           howmuchis.result = parseInt(response.data[0].price_usd) * howmuchis.selected.amount;
         })
+    },
+    getIcon: function(coin) {
+      return "node_modules/cryptocoins-icons/SVG/" + coin.icon;
+    },
+    checkValid: function() {
+
+    },
+    addCoin: function() {
+      howmuchis.inputs.push({coin:'placeholder'})
     }
   }
 });
 
-howmuchis.coinList = [
-  {
+howmuchis.coinList = [{
     id: 'bitcoin',
     desc: 'Bitcoin',
+    icon: 'BTC.svg',
   },
   {
     id: 'monero',
-    desc: 'Monero'
+    desc: 'Monero',
+    icon: 'XMR.svg',
   },
   {
     id: 'ethereum',
-    desc: 'Ethereum'
-  },
-  {
-    id: 'raiblocks',
-    desc: 'Raiblocks'
+    desc: 'Ethereum',
+    icon: 'ETH.svg',
   }
 ];
